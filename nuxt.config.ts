de98a7e64ca17ecd7ac1eebd08fa43709f9b59f7
Anshuman Komawar <anshumankomawar@gmail.com>
2024-08-07 23:50:41 -0700

    fix: changed dynamic url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,8 @@
 export default defineNuxtConfig({
 	runtimeConfig: {
 		public: {
-			url: process.env.VERCEL_URL
-				? `https://${process.env.VERCEL_URL}`
+			url: process.env.VERCEL_PROJECT_PRODUCTION_URL
+				? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
 				: "http://localhost:3000",
 		},
 	},
